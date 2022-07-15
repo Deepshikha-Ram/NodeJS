c788c077f096571f90c693febe2a81ea033ebad6
Deepshikha Ram <52650070+Deepshikha-Ram@users.noreply.github.com>
2022-07-15 12:33:49 +0530

    Create QRCodeInTerminal.js

---
+++ QRCodeInTerminal.js
@@ -0,0 +1,10 @@
+const QRCode = require("qrcode")
+const generateQR = async text =>{
+    try{
+        console.log(await QRCode.toString(text, {type:'terminal'}))
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+generateQR("http://yahoo.com/")
